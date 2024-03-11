0414858427a0f8cac3252d7dc2eda080da30ce32
End1essRage <end1essrage@mail.ru>
2024-03-11 19:41:42 +0300

    Добавил возможность конфигурирования приложения

--- cmd/identity/main.go
+++ cmd/identity/main.go
@@ -1,5 +1,18 @@
-package identity
+package main
+
+import (
+	"fmt"
+	"identity/internal/config"
+)
 
 func main() {
+	//инициализровать конфиг
+	cfg := config.MustLoad()
 
+	fmt.Println(cfg)
+	//инициализровать логгер
+
+	//инициализровать приложение
+
+	//запустить grpc сервер
 }
